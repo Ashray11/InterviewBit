0cd26079b7dae274a55b4972a05fdb44f5b17c03
Riya Shah <33380157+riyashah17@users.noreply.github.com>
2018-07-19 23:54:02 +0530

    Create Palindrome Integer.cpp

---
+++ Math/Palindrome Integer.cpp
@@ -0,0 +1,26 @@
+int Solution::isPalindrome(int A) {
+    
+    if(A<0){
+        return false;
+    }
+    
+    
+    int divisor = 1;
+    while(A/divisor>=10){
+        divisor*=10;
+    }
+    
+    while(A!=0){
+        int leading = A/divisor;
+        int trailing = A%10;
+        
+        if(leading!=trailing){
+            return false;
+        }
+        
+        A = (A%divisor)/10;
+        divisor = divisor/100;
+    }
+    
+    return true;
+}
