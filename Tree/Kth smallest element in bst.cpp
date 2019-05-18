23d98618e4efd17fe913c224cc6b4badf4d10c9b
Riya Shah <33380157+riyashah17@users.noreply.github.com>
2019-05-18 15:04:13 +0530

    Use preorder traversal

---
+++ Tree/Kth smallest element in bst.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int Solution::kthsmallest(TreeNode* A, int B) {
+    
+    if(A==NULL)
+        return 0;
+    
+    TreeNode* temp;  
+    stack <TreeNode* >s;
+    int count = 0;  
+    while(true){
+        if(A!=NULL){
+            s.push(A);
+            A = A->left;
+        }
+        else{
+            if(count==B)
+                break;
+            A = s.top();
+            s.pop();
+            count++;
+            temp = A;
+            A = A->right;
+        }
+    }
+    
+    return temp->val;
+}
