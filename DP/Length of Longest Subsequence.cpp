7bf8ebcbebb3597e2568d6371cbc6481d577f3a2
Riya Shah <33380157+riyashah17@users.noreply.github.com>
2019-05-20 15:50:57 +0530

    Create Length of Longest Subsequence.cpp

---
+++ DP/Length of Longest Subsequence.cpp
@@ -0,0 +1,34 @@
+int Solution::longestSubsequenceLength(const vector<int> &A) {
+    
+    if(A.size()==0)
+        return 0;
+    
+    int len = A.size();
+    
+    vector<int> inc(len,1);
+    vector<int> dec(len,1);
+    
+    for(int i=1;i<len;i++){
+        for(int j=0;j<i;j++){
+            if(A[j]<A[i])
+                inc[i] = max(inc[i],inc[j]+1);
+        }
+    }
+    
+    for(int i=len-2;i>=0;i--){
+        for(int j=len-1;j>i;j--){
+            if(A[j]<A[i])
+                dec[i] = max(dec[i],dec[j]+1);
+        }
+    }
+    
+    int max = 0; 
+    
+    for(int i=0;i<len;i++){
+        if(inc[i]+dec[i]-1 > max){
+            max = inc[i]+dec[i]-1;
+        }
+    }
+    
+    return max; 
+}
