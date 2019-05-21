1e16a2dd9527302796374176d37e5609332f42d2
Riya Shah <33380157+riyashah17@users.noreply.github.com>
2019-05-21 16:07:01 +0530

    Create Max Rectangule in Binary Matrix.cpp

---
+++ Max Rectangule in Binary Matrix.cpp
@@ -0,0 +1,72 @@
+//Max Rectangular Area in a Histogram
+int histogram(vector<int> v){
+    
+    stack <int> s;
+    int i = 0;
+    int len = v.size();
+    int area;
+    int max_area = 0;    
+    
+    while(i<len){
+        
+        if(s.empty() || v[i]>=v[s.top()]){
+            s.push(i);
+            i++;
+        }
+        else{
+            int t = s.top();
+            s.pop();
+            if(s.empty()){
+                area = v[t]*i;
+            }
+            else{
+                area = v[t]*(i-s.top()-1);
+            }
+            
+            if(max_area<area)
+                max_area = area;
+        }
+    }
+    
+    while(!s.empty()){
+        int t = s.top();
+        s.pop();
+        if(s.empty()){
+            area = v[t]*i;
+        }
+        else{
+            area = v[t]*(i-s.top()-1);
+        }
+        
+        if(max_area<area)
+            max_area = area;
+    }
+    
+    return max_area;
+}
+
+int Solution::maximalRectangle(vector<vector<int> > &A) {
+    
+    vector<int> v(A[0]);
+    int result = histogram(v);
+    int area;
+    int max_area = result;
+    
+    for(int i=1;i<A.size();i++){
+        for(int j=0;j<A[0].size();j++){
+            if(A[i][j]==0){
+                v[j] = 0;
+            }
+            else{
+                v[j] += A[i][j];
+            }
+        }
+        
+        area = histogram(v);
+        if(area>max_area)
+            max_area = area;
+    }
+    
+    return max_area;
+
+}
