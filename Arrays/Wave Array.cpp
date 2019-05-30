7476bed1ea8cc71bf55a97798bd0f27c09dedc8c
Riya Shah <33380157+riyashah17@users.noreply.github.com>
2019-05-30 15:12:43 +0530

    Update Wave Array.cpp

--- Arrays/Wave Array.cpp
+++ Arrays/Wave Array.cpp
@@ -2,13 +2,10 @@ vector<int> Solution::wave(vector<int> &A) {
     int n = A.size();
     sort(A.begin(),A.end());
     
-    for(int i=0;i<n;i=i+2){
-        if(i>0 && A[i]<A[i-1])
-            swap(A[i],A[i-1]);
-            
-        if(i<n-1 && A[i]<A[i+1])
-            swap(A[i],A[i+1]);
+    for(int i=0;i<n-1;i=i+2){
+        swap(A[i],A[i+1]);
     }
     
     return A;
 }
+
