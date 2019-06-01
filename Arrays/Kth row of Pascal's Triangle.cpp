6688d5aa0c7c0c02c1393674921c454bc58b1432
Riya Shah <33380157+riyashah17@users.noreply.github.com>
2019-06-01 14:47:38 +0530

    Update Kth row of Pascal's Triangle.cpp

--- Arrays/Kth row of Pascal's Triangle.cpp
+++ Arrays/Kth row of Pascal's Triangle.cpp
@@ -1,4 +1,5 @@
-vector<int> Solution::getRow(int n) {
+
+/*vector<int> Solution::getRow(int n) {
     int row = n+1;
     if(row==0)
         return vector <int> ();
@@ -9,4 +10,25 @@ vector<int> Solution::getRow(int n) {
             C = C*(row-i)/i;
         }
     return tri;
+}*/
+
+vector<int> Solution::getRow(int n) {
+    
+    int A = n;
+    vector<vector<int>> v(A+1);
+    for(int i=0;i<A+1;i++){
+        for(int j=0;j<=i;j++){
+            if(j==0 || j==i){
+                v[i].push_back(1);
+            }
+            else{
+                int x = v[i-1][j] + v[i-1][j-1];
+                v[i].push_back(x);
+            }
+        }
+    }
+    
+    return v[A];
+
 }
+
