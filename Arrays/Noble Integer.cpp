35142c081c542fda80dc62874b51e91bc78a15fe
Riya Shah <33380157+riyashah17@users.noreply.github.com>
2019-06-01 17:06:36 +0530

    Update Noble Integer.cpp

--- Arrays/Noble Integer.cpp
+++ Arrays/Noble Integer.cpp
@@ -2,18 +2,16 @@ int Solution::solve(vector<int> &A) {
     int n = A.size();
     int flag=-1;
     sort(A.begin(),A.end());
-    // for(int i=0;i<n;i++){
-    //     if(A[i]<0){
-    //         A[i] = -A[i];
-    //     }
-    // }
-    for(int i=0;i<n;i++){
-        if(A[i]==A[i+1])
-            continue;
+    
+    int i=0;
+    while(i<n){
+        while( i+1<n && A[i]==A[i+1])
+            i++;
         if(A[i] == n-i-1){
             flag = 1;
             break;
         }
+        i++;
     }
     
     if(flag==1)
