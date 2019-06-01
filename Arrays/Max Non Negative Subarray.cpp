08c6f6c6b83fd46896f58f8836165645bc3bb6a2
Riya Shah <33380157+riyashah17@users.noreply.github.com>
2019-06-01 13:03:09 +0530

    Update Max Non Negative Subarray.cpp

--- Arrays/Max Non Negative Subarray.cpp
+++ Arrays/Max Non Negative Subarray.cpp
@@ -1,37 +1,45 @@
 vector<int> Solution::maxset(vector<int> &A) {
-        int N = A.size();
-
-        long long mx_sum = 0;
-        long long cur_sum = 0;
-        int mx_range_left = -1;
-        int mx_range_right = -1;
-        int cur_range_left = 0;
-        int cur_range_right = 0;
-
-        while(cur_range_right < N) {
-            if(A[cur_range_right] < 0) {
-                cur_range_left = cur_range_right + 1;
-                cur_sum = 0;
-            } else {
-                cur_sum += (long long)A[cur_range_right];
-                if(cur_sum > mx_sum) {
-                    mx_sum = cur_sum;
-                    mx_range_left = cur_range_left;
-                    mx_range_right = cur_range_right + 1;
-                } else if(cur_sum == mx_sum) {
-                    if(cur_range_right + 1 - cur_range_left > mx_range_right - mx_range_left) {
-                        mx_range_left = cur_range_left;
-                        mx_range_right = cur_range_right + 1;
-                    }
-                }
+    
+    long long sum = 0;
+    int start = -1;
+    int len = 0;
+    long long max_sum = INT_MIN;
+    int max_len = INT_MIN;
+    int min_start = INT_MAX;
+    for(int i=0;i<A.size();i++){
+        if(A[i]>=0){
+            sum = sum + (long long)A[i];
+            if(start == -1){
+                start = i;
             }
-            cur_range_right++;
+            len++;
+            
         }
-        vector<int> ans;
-        if(mx_range_left == -1 || mx_range_right == -1)
-                return ans;
-
-        for(int i = mx_range_left; i < mx_range_right; ++i)
-                ans.push_back(A[i]);
-        return ans;
+        if(sum>max_sum){
+            max_sum = sum;
+            max_len = len;
+            min_start = start;
+        }
+        else if(sum==max_sum && len>max_len){
+            max_len = len;
+            min_start = start;
+        }
+        if(A[i]<0){
+            sum = 0;
+            start = -1;
+            len = 0;
+        } 
+    }
+    
+    vector<int> v;
+    if(sum == 0 ){
+        //v.push_back(0);
+        return v;
+    }
+    for(int i=min_start;i<min_start+max_len;i++){
+        v.push_back(A[i]);
+    }
+    
+    return v;
 }
+
